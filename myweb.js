f61762e80e6bc9478f94ce867e406e80dfbf311c
0-revanth <168257954+0-revanth@users.noreply.github.com>
2024-08-17 22:34:05 +0530

    RevanthrajuOruganti

---
+++ myweb.js
@@ -0,0 +1,24 @@
+function python(){
+    window.open("https://www.credly.com/badges/90179914-d6b2-4192-b0ad-8cc1ca8b271e/public_url","_blank");
+}
+
+function cyber(){
+    window.open("https://www.credly.com/badges/4ebb82e8-c1eb-4c82-b22a-80c96014129e/public_url","_blank");
+}
+function prompt(){
+    window.open("https://www.credly.com/badges/92b93c13-7f7f-4a11-8efb-bd185267294e/public_url","_blank");
+}
+
+function ccna(){
+    window.open("https://www.credly.com/badges/439941c5-3e4b-486e-b730-ecfc9831750a/public_url","_blank");
+}
+
+function resweb(){
+    window.open("https://www.freecodecamp.org/certification/fccfce8ce71-0218-46dc-964d-7c86cb3ea147/responsive-web-design","_blank");
+}
+function linux(){
+    window.open("https://courses.edx.org/certificates/7c11a982a2644533912f8a6c3c5d3dd2","_blank");
+}
+function ethical(){
+    window.open("https://simpli-web.app.link/e/A18gSJAi8Lb","_blank");
+}
